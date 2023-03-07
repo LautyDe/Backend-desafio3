aa3e6f7e38c516dedcba22b30295f9e32ee1bc98
Lautaro Demonte <lauty.d.p@gmail.com>
2023-03-07 01:44:37 -0300

    agrego endpoint para buscar producto por su id

--- app.js
+++ app.js
@@ -39,7 +39,22 @@ app.get("/products", async (req, res) => {
     }
   } catch (error) {
     console.log(`Error obteniendo los productos: ${error.message}`);
-    res.status(500).json({ error: "Error obteniendo los productos" });
+    res.status(500);
+  }
+});
+
+app.get("/products/:pid", async (req, res) => {
+  try {
+    const { pid } = req.params;
+    const product = await productManager.getById(parseInt(pid));
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ error: "Producto no encontrado" });
+      throw new Error(`Producto con id ${pid} no encontrado`);
+    }
+  } catch (error) {
+    console.log(`Error obteniendo producto por su id: ${error.message}`);
   }
 });
 
